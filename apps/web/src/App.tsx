78e7208412729e8384d6a8d21360bd254a469ac9
Krish Rathor <krishrathor18@gmail.com>
2024-12-10 14:15:52 +0530

    checkpoint before implementing slash command

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,10 +3,12 @@ import { TipTapEditor } from "./components/editor/TipTapEditor";
 
 function App() {
   return (
-    <div>
+    <div className="h-fit min-h-[100vh] bg-[#18181B] text-white" >
       <div>
-        <h1>Tip Tap Editor in React</h1>
-        <TipTapEditor />
+        <h1 className="text-red-500 text-2xl" >Tip Tap Editor in React</h1>
+        <div className="text-white" >
+          <TipTapEditor />
+        </div>
       </div>
     </div>
   );
--- apps/web/src/components/editor/TipTapEditor.tsx
+++ apps/web/src/components/editor/TipTapEditor.tsx
@@ -1,31 +1,51 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import React from "react";
-import BulletList from '@tiptap/extension-bullet-list'
-import Document from '@tiptap/extension-document'
-import ListItem from '@tiptap/extension-list-item'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
+import BulletList from "@tiptap/extension-bullet-list";
+import Document from "@tiptap/extension-document";
+import ListItem from "@tiptap/extension-list-item";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import Blockquote from "@tiptap/extension-blockquote";
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
+import { all, createLowlight } from 'lowlight'
+import Heading from '@tiptap/extension-heading'
+import Image from '@tiptap/extension-image'
+import { Color } from '@tiptap/extension-color'
+import Placeholder from '@tiptap/extension-placeholder'
+
+const lowlight = createLowlight(all);
 
 export const TipTapEditor: React.FC = () => {
-    const editor = useEditor({
-        extensions: [
-            StarterKit.configure(),
-            Document,
-            BulletList,
-            ListItem,
-            Paragraph,
-            Text
-        ],
-        content: `
-            <ul>
-                <li>hi there</li>
-                <li>bi there</li>
-            </ul>
-        `
-    });
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure(),
+      Document,
+      BulletList,
+      ListItem,
+      Paragraph,
+      Text,
+      Color,
+      Image,
+      Blockquote,
+      CodeBlockLowlight.configure({
+        lowlight,
+      }),
+      Heading.configure({
+        levels: [1, 2, 3],
+      }),
+      Placeholder.configure({
+        placeholder: 'Type / for suggestions'
+      })
+    ],
+    content: `
+        `,
+  });
 
-    return (
-        <EditorContent editor={editor} />
-    )
-}
+  return (
+    <EditorContent
+      className="tiptap-editor prose text-white mx-auto border border-white rounded-md"
+      editor={editor}
+    />
+  );
+};
